36bbe49d17202626cbd27440d84f298985a550d0
yannleboss <Yann.riani@gmail.com>
2023-03-10 10:27:46 +0100

    debut de la generation

--- Assets/Generation de terrain/CreateTerrain.cs
+++ Assets/Generation de terrain/CreateTerrain.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class CreateTerrain : MonoBehaviour
 {
+    private Mesh p_mesh;
+    private Vector3[] p_vertices;
+    private int[] p_triangles;
+    private Color32[] p_colors;
+    private Vector3[] p_normals;
+    public int Dimension;
+    public int Resolution;
+    public bool CentrerPivot;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if(Resolution%2 == 1)
+        {
+            Debug.LogError("Resolution must be an power of 2");
+        }
     }
 
-    void GenereateTerrain()
+    void Start()
     {
+        GenereateTerrain();
+    }
 
+    private void GenereateTerrain()
+    {
+        p_mesh = new Mesh();
+        p_mesh.name = "MyProceduralTerrain";
+        GetComponent<MeshFilter>().mesh = p_mesh;
     }
 }
