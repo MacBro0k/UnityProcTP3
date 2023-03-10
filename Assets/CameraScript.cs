549bdd449cd3c834c2e000115d6f272c332320a2
William Choquet <william.choquet@etu.uca.fr>
2023-03-10 10:05:36 +0100

    CameraScript + CreateTerrain

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Orbit : MonoBehaviour
+public class CameraScript : MonoBehaviour
 {
     public Transform target;
     void Update()
